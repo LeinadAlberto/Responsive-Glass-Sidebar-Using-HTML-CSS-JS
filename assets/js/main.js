dde451f739c933f1364f44089ec56606f4070115
Alberto <dany.canaviri2022@gmail.com>
2026-01-02 23:36:08 -0400

    Sidebar, completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,14 @@
 /*=============== SHOW SIDEBAR ===============*/
+const showSidebar = (toggleId, sidebarId) => {
+    const toggle = document.getElementById(toggleId),
+          sidebar = document.getElementById(sidebarId)
 
+    toggle.addEventListener('click', () => {
+        sidebar.classList.toggle('show-sidebar')
+    })
+}
+
+showSidebar('header-toggle', 'sidebar')
 
 /*=============== SHOW SIDEBAR LIST ===============*/
 
